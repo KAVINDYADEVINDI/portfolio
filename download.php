a3b255552c3d6199281592a10edbc2976de87969
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-05-17 10:52:50 -0700

    backemd11

--- download.php
+++ download.php
@@ -1,24 +1,24 @@
 <?php
 print_r($_GET['file']);
-//  if(!empty($_GET['file'])){
-//    $filename=basename($_GET['file']);
-//    $filepath='destination/'.$filename;
-//    if(!empty($filename) && file_exists($filepath)){
+ if(!empty($_GET['file'])){
+   $filename=basename($_GET['file']).".pdf";
+   $filepath='destination/'.$filename;
+   if(!empty($filename) && file_exists($filepath)){
 
-//     //define headers
-//     header("Cache-Control:public");
-//     header("Content-Description:FIle Transfer");
-//     header("Content-Disposition: attachment; filename=$filename");
-//     header("Content-Type:application/zip");
-//     header("Content-Transfer-Emcoding:binary");
+    //define headers
+    header("Cache-Control:public");
+    header("Content-Description:FIle Transfer");
+    header("Content-Disposition: attachment; filename=$filename");
+    header("Content-Type:application/zip");
+    header("Content-Transfer-Emcoding:binary");
 
-//     readfile($filepath);
-//     exit;
-
-//    }
-//     else{
-//       echo "This file does not exist";
-//     }
-//    }
-//  }
+    readfile($filepath);
+    exit;
+    
+   }
+    else{
+      echo "This file does not exist";
+    }
+   }
+ }
 ?>
