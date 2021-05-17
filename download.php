beae29f441915c7ae468b9d47c1714437b20814c
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-05-17 11:15:32 -0700

    backend13

--- download.php
+++ download.php
@@ -1,23 +1,34 @@
 <?php
-print_r($_GET['file']);
- if(!empty($_GET['file'])){
-   $filename=basename($_GET['file']);
-   $filepath='destination/'.$filename;
-   if(!empty($filename) && file_exists($filepath)){
+// print_r($_GET['file']);
+//  if(!empty($_GET['file'])){
+//    $filename=basename($_GET['file']);
+//    $filepath='destination/'.$filename;
+//    if(!empty($filename) && file_exists($filepath)){
 
-    //define headers
-    header("Cache-Control:public");
-    header("Content-Description:FIle Transfer");
-    header("Content-Disposition: attachment; filename=$filename");
-    header("Content-Type:application/zip");
-    header("Content-Transfer-Emcoding:binary");
+//     //define headers
+//     header("Cache-Control:public");
+//     header("Content-Description:FIle Transfer");
+//     header("Content-Disposition: attachment; filename=$filename");
+//     header("Content-Type:application/zip");
+//     header("Content-Transfer-Emcoding:binary");
 
-    readfile($filepath);
-    exit;
-   }
-    else{
-      echo "This file does not exist";
-    }
-   }
- }
+//     readfile($filepath);
+//     exit;
+//    }
+//     else{
+//       echo "This file does not exist";
+//     }
+//    }
+//  }
+$file=$_GET['file'].".pdf";
+
+header("content-disposition:attachment;filename" . urlencode($file));
+
+$fb=fopen($file,"r");
+
+while(!feof($fb)){
+    echo fread($fb,8192);
+    flush();
+}
+fclose($fb);
 ?>
